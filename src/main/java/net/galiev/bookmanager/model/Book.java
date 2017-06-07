74826636f18cedf1856d3780a60033a552d676aa
proff77 <azat_galiev@mail.ru>
2017-06-07 13:15:24 +0300

    Initial commit

---
+++ src/main/java/net/galiev/bookmanager/model/Book.java
@@ -0,0 +1,64 @@
+package net.galiev.bookmanager.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class Book {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "isAdmin")
+    private Boolean isAdmin;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Boolean getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(Boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", isAdmin='" + isAdmin + '\'' +
+                '}';
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Galiev</title>
+  </head>
+  <body>
+  <h3>Galiev presents</h3>
+  <br />
+  <a href="<c:url value="/books"/>" target="_blank">Users list</a>
+  <br />
+  </body>
+</html>
