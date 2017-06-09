376e98ec312adbb42187808ae22552aeb149b1ea
proff77 <azat_galiev@mail.ru>
2017-06-09 13:34:53 +0300

    worked2

--- src/main/java/net/galiev/crud/service/UserServiceImpl.java
+++ src/main/java/net/galiev/crud/service/UserServiceImpl.java
@@ -16,31 +16,26 @@ public class UserServiceImpl implements UserService {
         this.userDao = userDao;
     }
 
-    @Override
     @Transactional
     public void addUser(User user) {
         this.userDao.addUser(user);
     }
 
-    @Override
     @Transactional
     public void updateUser(User user) {
         this.userDao.updateUser(user);
     }
 
-    @Override
     @Transactional
     public void removeUser(int id) {
         this.userDao.removeUser(id);
     }
 
-    @Override
     @Transactional
     public User getUserById(int id) {
         return this.userDao.getUserById(id);
     }
 
-    @Override
     @Transactional
     public List<User> listUsers() {
         return this.userDao.listUsers();
